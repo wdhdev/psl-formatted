6e2eb594cbc304fb9ed1a76087f9e018e05010d8
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-11-13 17:00:48 +0800

    upload code

---
+++ README.md
@@ -0,0 +1,10 @@
+# PSL Minified
+A project with the goal of minifying the PSL for projects that want a smaller copy of public_suffix_list.dat.
+
+**This project is NOT associated with the [Public Suffix List](https://publicsuffix.org). It is a community-run project.**
+
+> [!NOTE]
+> We do not modify the PSL in any way, we simply remove the comments from the original file. You can see the minifying script [here](scripts/minify.js).
+
+## Pulling the file
+You can pull the file from https://psl-min.hrsn.dev/list/public_suffix_list.min.dat
---
+++ scripts/minify.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+const file = "public_suffix_list.dat";
+
+fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+        console.error("Error reading the file:", err);
+        return;
+    }
+
+    const requiredComments = [
+        "// ===BEGIN ICANN DOMAINS===",
+        "// ===END ICANN DOMAINS===",
+        "// ===BEGIN PRIVATE DOMAINS===",
+        "// ===END PRIVATE DOMAINS==="
+    ];
+
+    const startsWithComments = ["// VERSION:", "// COMMIT:"];
+
+    // Minify the data
+    const minifiedData = data
+        .split("\n")
+        .map((line) => {
+            if (requiredComments.some((comment) => line.startsWith(comment))) {
+                return line;
+            }
+
+            if (startsWithComments.some((comment) => line.startsWith(comment))) {
+                return line;
+            }
+
+            return line.replace(/\/\/.*/, "").trim();
+        })
+        .filter((line) => line.length > 0)
+        .join("\n");
+
+    // Create the list directory if it doesn't exist
+    if (!fs.existsSync("list")) {
+        fs.mkdirSync("list");
+    }
+
+    // Write the minified data to a new file
+    fs.writeFile("list/public_suffix_list.min.dat", minifiedData, "utf8", (writeErr) => {
+        if (writeErr) {
+            console.error("Error writing the file:", writeErr);
+        } else {
+            console.log(`${((data.length - minifiedData.length / data.length) * 100).toFixed(2)}%`);
+        }
+    });
+});
